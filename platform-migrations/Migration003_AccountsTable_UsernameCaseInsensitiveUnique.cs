f527e18af2a056750b7b911e4680d1410cd6f8d2
Warty <ItzWarty@gmail.com>
2015-09-11 13:03:40 -0700

    Accounts table index is now case-sensitive.

---
+++ platform-migrations/Migration003_AccountsTable_UsernameCaseInsensitiveUnique.cs
@@ -0,0 +1,16 @@
+﻿using FluentMigrator;
+
+namespace Dargon.Platform.Migrations {
+   [Migration(201509111145)]
+   public class Migration003_AccountsTable_UsernameCaseInsensitiveUnique : Migration {
+      public override void Up() {
+         Delete.Index("IX_accounts_username").OnTable("accounts");
+         Execute.Sql("CREATE UNIQUE INDEX IX_accounts_username ON accounts(LOWER(username))");
+      }
+
+      public override void Down() {
+         Delete.Index("IX_accounts_username").OnTable("accounts");
+         Alter.Column("username").OnTable("accounts").AsString(32).Unique();
+      }
+   }
+}
